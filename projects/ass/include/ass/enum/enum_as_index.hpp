33bc6d46583e32f11c3dac929785ce3d021ef04e
Konstantin Lazukin <konstantin.lazukin@gmail.com>
2023-04-24 02:45:29 +0300

    Sparse unsigned enum index converter

--- projects/ass/include/ass/enum/enum_as_index.hpp
+++ projects/ass/include/ass/enum/enum_as_index.hpp
@@ -1,8 +1,42 @@
 #pragma once
 
+#include <array>
 #include <cassert>
 #include <cstddef>
+#include <limits>
 #include <type_traits>
+
+#include "ass/fixed_bitset.hpp"
+#include "ass/fixed_unordered_map.hpp"
+
+namespace ass::enum_as_index_detail
+{
+template <size_t MapCapacity, typename Hasher, typename T, size_t ElementsCount>
+constexpr FixedUnorderedMap<MapCapacity, T, size_t, Hasher> MakeEnumToIndexMap(
+    const std::array<T, ElementsCount>& elements)
+{
+    FixedUnorderedMap<MapCapacity, T, size_t, Hasher> result{};
+    for (size_t index = 0; index != elements.size(); ++index)
+    {
+        result.Add(elements[index], index);
+    }
+    return result;
+}
+template <typename T>
+struct ConstexprEnumHasher
+{
+    constexpr ConstexprEnumHasher() = default;
+    constexpr size_t operator()(const T value) const noexcept
+    {
+        auto myabs = [](auto v)
+        {
+            return v < 0 ? -v : v;
+        };
+        using U = std::underlying_type_t<T>;
+        return static_cast<size_t>(myabs(static_cast<U>(value)));
+    }
+};
+}  // namespace ass::enum_as_index_detail
 
 namespace ass
 {
@@ -51,6 +85,36 @@ struct EnumIndexConverter_Continuous
 
     static_assert(std::is_enum_v<T>);
     static_assert(kBegin <= kEnd);
+};
+
+template <
+    typename T,
+    auto values_fn,
+    size_t CustomMapCapacity = 0,
+    typename Hasher = enum_as_index_detail::ConstexprEnumHasher<T>>
+struct EnumIndexConverter_Sparse
+{
+    using U = std::underlying_type_t<T>;
+    static constexpr auto kEnumValues = values_fn();
+    static constexpr size_t kMapCapacity = CustomMapCapacity == 0 ? kEnumValues.size() * 2 : CustomMapCapacity;
+    static constexpr auto kEnumToIndexMap = enum_as_index_detail::MakeEnumToIndexMap<kMapCapacity, Hasher>(kEnumValues);
+    static_assert(kMapCapacity > kEnumValues.size());
+    static constexpr bool IsImplemented()
+    {
+        return true;
+    }
+    static constexpr size_t ConvertEnumToIndex(const T enum_value)
+    {
+        return kEnumToIndexMap.Get(enum_value);
+    }
+    static constexpr T ConvertIndexToEnum(const size_t index)
+    {
+        return kEnumValues[index];
+    }
+    static constexpr size_t GetElementsCount()
+    {
+        return kEnumValues.size();
+    }
 };
 
 }  // namespace ass
--- projects/ass_tests/enum/enum_as_index.cpp
+++ projects/ass_tests/enum/enum_as_index.cpp
@@ -1,5 +1,7 @@
 #include "ass/enum/enum_as_index.hpp"
 
+#include <array>
+
 #include "gtest/gtest.h"
 
 enum class ContinuousUnsigned : unsigned
@@ -62,4 +64,56 @@ TEST(EnumIndexConverter, ContinuousSigned)
     ASSERT_EQ(CV::ConvertIndexToEnum(1), E::B);
     ASSERT_EQ(CV::ConvertIndexToEnum(2), E::C);
     ASSERT_EQ(CV::ConvertIndexToEnum(3), E::D);
+}
+
+enum class SparseEnumeration
+{
+    A = 0,
+    B = 13,
+    C = 22,
+    D = 23,
+    E = 42,
+    F = 48,
+    G = 59
+};
+
+inline constexpr auto get_sparse_enum_values()
+{
+    std::array<SparseEnumeration, 7> values{
+        SparseEnumeration::A,
+        SparseEnumeration::B,
+        SparseEnumeration::C,
+        SparseEnumeration::D,
+        SparseEnumeration::E,
+        SparseEnumeration::F,
+        SparseEnumeration::G};
+
+    return values;
+}
+
+template <>
+struct ass::EnumIndexConverter<SparseEnumeration>
+    : ass::EnumIndexConverter_Sparse<SparseEnumeration, get_sparse_enum_values>
+{
+};
+
+TEST(EnumIndexConverter, SparseUnsigned)
+{
+    using E = SparseEnumeration;
+    using CV = ass::EnumIndexConverter<E>;
+    ASSERT_EQ(CV::GetElementsCount(), 7);
+    ASSERT_EQ(CV::ConvertEnumToIndex(E::A), 0);
+    ASSERT_EQ(CV::ConvertEnumToIndex(E::B), 1);
+    ASSERT_EQ(CV::ConvertEnumToIndex(E::C), 2);
+    ASSERT_EQ(CV::ConvertEnumToIndex(E::D), 3);
+    ASSERT_EQ(CV::ConvertEnumToIndex(E::E), 4);
+    ASSERT_EQ(CV::ConvertEnumToIndex(E::F), 5);
+    ASSERT_EQ(CV::ConvertEnumToIndex(E::G), 6);
+    ASSERT_EQ(CV::ConvertIndexToEnum(0), E::A);
+    ASSERT_EQ(CV::ConvertIndexToEnum(1), E::B);
+    ASSERT_EQ(CV::ConvertIndexToEnum(2), E::C);
+    ASSERT_EQ(CV::ConvertIndexToEnum(3), E::D);
+    ASSERT_EQ(CV::ConvertIndexToEnum(4), E::E);
+    ASSERT_EQ(CV::ConvertIndexToEnum(5), E::F);
+    ASSERT_EQ(CV::ConvertIndexToEnum(6), E::G);
 }
